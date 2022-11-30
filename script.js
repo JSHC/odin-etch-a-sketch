dcc1edafae8c57ca4dcf9aec8915acb5d5a008c5
JSHC <johan.hansson.carlberg@gmail.com>
2022-11-30 20:29:38 +0100

    Add grid resize functionality

--- script.js
+++ script.js
@@ -1,14 +1,37 @@
 const container = document.querySelector(".grid-container");
+
 const clearButton = document.querySelector("#clear");
 clearButton.addEventListener('click', clearBoxes);
+
+const setGridButton = document.querySelector("#set-grid");
+setGridButton.addEventListener('click', setGridSize);
+
 const divs = [];
-for (let i = 0; i < 16; i++) {
-    for (let j = 0; j < 16; j++) {
-        const newDiv = document.createElement("div");
-        newDiv.classList.add("grid-box");
-        container.appendChild(newDiv);
-        newDiv.addEventListener('click', onBoxHover);
-        divs.push(newDiv);
+generateGrid(10);
+
+function generateGrid(size) {
+    if (size <= 0) {
+        return;
+    }
+
+    if (divs.length > 0) {
+        divs.forEach(div => {
+            div.remove();
+        });
+        divs.length = 0;
+    }
+
+    container.style.gridTemplateColumns = `repeat(${size}, 1fr)`
+    container.style.gridTemplateRows = `repeat(${size}, 1fr)`
+
+    for (let i = 0; i < size; i++) {
+        for (let j = 0; j < size; j++) {
+            const newDiv = document.createElement("div");
+            newDiv.classList.add("grid-box");
+            container.appendChild(newDiv);
+            newDiv.addEventListener('click', onBoxHover);
+            divs.push(newDiv);
+        }
     }
 }
 
@@ -25,4 +48,16 @@ function clearBoxes() {
     divs.forEach(div => {
         div.style.backgroundColor = "#fff";
     });
+}
+
+function setGridSize() {
+    const newSize = Number(
+        prompt("Enter number of squares per side (limit: 100):")
+        );
+    
+    if (Number.isNaN(newSize)) {
+        return;
+    }
+
+    generateGrid(newSize);
 }
