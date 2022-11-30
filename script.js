ab9812a322e9d7af6de35dbd9ded82d73d3dc4c3
JSHC <johan.hansson.carlberg@gmail.com>
2022-11-30 19:56:35 +0100

    Add initial coloring functionality

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
 const container = document.querySelector(".grid-container");
-
+const divs = [];
 for (let i = 0; i < 16; i++) {
     for (let j = 0; j < 16; j++) {
         const newDiv = document.createElement("div");
         newDiv.classList.add("grid-box");
         container.appendChild(newDiv);
+        newDiv.addEventListener('click', onBoxHover);
+        divs.push(newDiv);
     }
+}
+
+function onBoxHover(e) {
+    console.log(e.target);
+    e.target.style.backgroundColor = "#000";
+}
+
+function clearBoxes() {
+    if (divs.length === 0) {
+        return;
+    }
+
+    divs.forEach(div => {
+        div.style.backgroundColor = "#fff";
+    });
 }
